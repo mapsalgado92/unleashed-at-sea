dd4993d5357b8653d5e4e2985beae27896e1d722
Mario Salgado <mapsalgado92@gmail.com>
2021-06-02 02:15:16 +0100

    pre-deploy2

--- comps/Footer.js
+++ comps/Footer.js
@@ -4,10 +4,13 @@ const Footer = () => {
   return (
     <footer>
       <Jumbotron className="d-flex align-items-center justify-content-center bg-dark text-center" bg="dark" style={{
-        minHeight: "20vh",
+        minHeight: "30vh",
       }} fluid>
         <Container>
           <h1>What are you waiting for?</h1>
+          <a className="mx-4 display-3" target="blank" href="https://www.facebook.com/unleashedatsea"><i className="fa fa-facebook-square text-light" /></a>
+          <a className="mx-4 display-3" target="blank" href="https://www.instagram.com/unleashedatsea"><i className="fa fa-instagram text-light" /></a>
+          <p id="signature" className="mt-3 text-light">Developed by | <span className="text-secondary">MarioJS</span></p>
         </Container>
       </Jumbotron>
     </footer>
--- pages/index.js
+++ pages/index.js
@@ -24,6 +24,10 @@ export default function Home() {
         <meta name="keywords" content="unleashed at sea festival boats waves lisbon" />
         <link rel="icon" href="/favicon.ico" />
         <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css" rel="stylesheet"></link>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css"
+          integrity="sha512-HK5fgLBL+xu6dm/Ii3z4xhlSUyZgTT9tuc/hSrtw6uzJOvgRr2a9jyxxT1ely+B+xFAmJKVSTbpM/CuL7qxO8w=="
+          crossorigin="anonymous" />
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"></link>
         <link rel="preconnect" href="https://fonts.gstatic.com" />
         <link href="https://fonts.googleapis.com/css2?family=Patua+One&display=swap" rel="stylesheet" />
       </Head>
@@ -45,7 +49,7 @@ export default function Home() {
               <h1 className="d-none">Unleashed at Sea, The Ultimate Boat Party in Lisbon</h1>
               <Alert className="m-auto card-overlay p-2 mt-2" rounded>
                 <h2 className="mb-0 h3 ">Sunday, 27.06.2021</h2>
-                <h2 className="h3 mb-0">3pm - 7pm</h2>
+                <h2 className="h3 mb-0">14:30 - 18:30</h2>
               </Alert>
 
             </Container>
@@ -62,7 +66,7 @@ export default function Home() {
                 <Col md={6} className="px-5">
                   <Image src={item.image} height="130em" alt={item.title} />
                   <h3 className="header-text text-warning">{item.title}</h3>
-                  <p>{item.text}</p>
+                  <p className="px-5">{item.text}</p>
                 </Col>
               )}
             </Row>
@@ -92,7 +96,7 @@ export default function Home() {
         <section className="text-center my-4">
           <a id="boatsSection" style={{ position: "relative", top: "-5em", display: "hidden" }}></a>
           <Container>
-            <h2 className="subtitle-text text-warning">Our Boat Selection</h2>
+            <h2 className="subtitle-text text-warning">Our Party Boats</h2>
             <h3 className="header-text text-dark">Learn more about the boats at our disposal. Each boat is unique and they have different boarding docks (see description). Be decisive and make a reservation if you find the ideal one for you and your crew!</h3>
             <List boats={boats} setSelectedBoat={setSelectedBoat} />
           </Container>
@@ -134,8 +138,8 @@ export default function Home() {
 
 const content = {
   hero1: {
-    title: "Dummy Content",
-    subtitle: "- Dummy Content -",
+    title: "We are not here to take part, We are here to takeover!",
+    subtitle: "The Unleashed at Sea ",
     text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris porttitor dui quis finibus ultrices. Proin at massa metus. Integer mollis eleifend turpis, eget commodo ante dapibus in. Quisque vulputate nec felis id egestas. In viverra magna vitae pulvinar blandit. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Aenean ornare sapien dolor, vitae vulputate enim vulputate ac."
   },
   howTo: {
